d43e25b022f95f68882feef930555e53f2d3b231
Ndubuisi Onyemenam <onyemenamndu@gmail.com>
2018-10-04 08:35:19 +0100

    Add set up files for posts module

--- index.php
+++ index.php
@@ -1,35 +1,105 @@
-<?php
-/**
- * @author           Pierre-Henry Soria <phy@hizup.uk>
- * @copyright        (c) 2015-2017, Pierre-Henry Soria. All Rights Reserved.
- * @license          Lesser General Public License <http://www.gnu.org/copyleft/lesser.html>
- * @link             http://hizup.uk
- */
+<?php require_once('./private/initialize.php') ?>
+<?php require_once(SHARED_PATH .'/index_html_header.php'); ?>
 
-// Note, the script requires PHP 5.5 or higher
-namespace TestProject;
+<body id="page-top" data-spy="scroll" data-target=".navbar">
 
-use TestProject\Engine as E;
-
-if (version_compare(PHP_VERSION, '5.5.0', '<'))
-    exit('Your PHP version is ' . PHP_VERSION . '. The script requires PHP 5.5 or higher.');
-if (!extension_loaded('mbstring'))
-    exit('The script requires "mbstring" PHP extension. Please install it.');
+  <?php require_once(SHARED_PATH .'/public_header.php'); ?>
+  <?php require_once(SHARED_PATH .'/public_sub_header.php'); ?>      
 
 
-// Set constants (root server path + root URL)
-define('PROT', (!empty($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) == 'on') ? 'https://' : 'http://');
-define('ROOT_URL', PROT . $_SERVER['HTTP_HOST'] . str_replace('\\', '', dirname(htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES))) . '/'); // Remove backslashes for Windows compatibility
-define('ROOT_PATH', __DIR__ . '/');
+    <div class="container">
+        
+            <div class="col content-middle col-75">
+        <div class="content-dark">
+            
+            <div class="content-light">
+                
+                <!--posts -->
+                  <ul class="forum-cat">
 
-try
-{
-    require ROOT_PATH . 'Engine/Loader.php';
-    E\Loader::getInstance()->init(); // Load necessary classes
-    $aParams = ['ctrl' => (!empty($_GET['p']) ? $_GET['p'] : 'blog'), 'act' => (!empty($_GET['a']) ? $_GET['a'] : 'index')]; // I use the new PHP 5.4 short array syntax
-    E\Router::run($aParams);
-}
-catch (\Exception $oE)
-{
-    echo $oE->getMessage();
-}
+                <?php require_once(SHARED_PATH .'/public_posts.php'); ?>  
+
+                <?php require_once(SHARED_PATH .'/public_navigation.php'); ?>
+                       
+                  </ul>
+                
+            </div>
+
+         
+
+            <!-- Headlines -->
+ 
+   <?php 
+         
+         if(!isset($query_string)) {
+            
+            require_once(SHARED_PATH .'/public_headlines.php');
+         }
+         else {
+            // need this div to keep adverts in place
+            echo "<div class=\"content-light headlines\" id=\"most-viewed\">";
+         }
+
+    
+   ?>            
+
+ 
+                                     
+                   
+
+
+               
+            </div>
+
+        </div>
+    </div>
+                
+            <aside class="col col-30 sidebar sidebar-right">
+    <div class="content-dark">
+        <h4>Sponsored ads</h4>
+                                                                                                    <div class="widget">
+            <!--a href="c.jumia.io/?a=102624&c=624&p=r&E=kkYNyk2M4sk%3d&utm_source=cake&utm_medium=affiliation&utm_campaign=102624&utm_term=" target="_blank">
+                <img src="https://affiliates.jumia.com/banners/Jumia Nigeria/EverythingonJumia/320x480.jpg"/></a-->
+                        </div>
+                        <br>
+         <div class="widget">
+                        <a href="com/article/12505"><img src="#"/></a>
+                        </div>
+                        <br>
+                                                        
+    </div>
+</aside>  
+
+                
+            <aside class="col col-30 sidebar sidebar-right">
+    <div class="content-dark">
+        <h4>Sponsored</h4>
+                                                                                                    <div class="widget">
+<a href="wqwqwqw" target="_blank">
+<div class="ad" style="border: 1px #31a931 solid; margin-bottom:15px; width: 100%;" >
+<img src="info/addimages/201803192305353741levcon_advirtisement.png"  style="width:100%;">
+</div>
+</a>
+  
+                        </div>
+                        <br>
+                                                                                                                                <div class="widget">
+                         <!--a href="https://blogsng.com/" target="_blank">
+<img src="#" /></a-->
+                        </div>
+                        <br>
+                                                        
+    </div>
+</aside>  
+                
+     </div>    
+<!--end of index.php-->
+
+
+
+ 
+
+ <?php require_once(SHARED_PATH .'/public_top_footer.php'); ?>
+ <?php require_once(SHARED_PATH .'/public_footer.php'); ?>
+
+    
---
+++ private/db_credentials.php
@@ -0,0 +1,8 @@
+<?php
+
+define("DB_SERVER", "localhost");
+define("DB_USER", "add yours");
+define("DB_PASS", "add yours");
+define("DB_NAME", "oprextra");
+
+?>
